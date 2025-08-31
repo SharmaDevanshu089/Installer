accf981436b082356f03f0641a16c9a1c969cd10
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-31 17:01:27 +0530

    Created a stable log file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use directories;
 use std::{fmt::format, io, path::PathBuf, process::Output};
 use reqwest;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 const URL:&str = "https://github.com/SharmaDevanshu089/AutoCrate/releases/download/v0.9.7/autocrate-windows-x86-64.zip";
 const SRS:&str = "There has been a Serious error with the program please use a different version. Crashing";
+const LOGFILE:&str = "install.log";
 fn main() {
     let out = String::from(get_directories("tmp").to_string_lossy());
     println!("{}",out);
@@ -20,6 +23,6 @@ pub fn get_directories(type_of:&str) -> PathBuf{
     return out;
 }
 fn log_error(text:&str){
-    println!("{}" , text);
-    panic!();    
+    let mut log_file = OpenOptions::new().create(true).append(true).open(LOGFILE).expect("Unable to Create Log File");
+    writeln!(&mut log_file,"{}",text).expect("Unable to write a error into log file");
 }
