2cc4bca24e7d53d34a370e1f33a2e9c2df08e133
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-09-01 16:51:26 +0530

    Updated for v0.9.9

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use directories;
 use reqwest::blocking::Response;
 use std::fs;
-use std::{fmt::format, io, path::PathBuf, process::Output};
+use std::path::PathBuf;
 use reqwest;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -13,7 +13,7 @@ use zip::ZipArchive;
 use std::env;
 use std::io::copy;
 
-const URL:&str = "https://github.com/SharmaDevanshu089/AutoCrate/releases/download/v0.9.7i/v0.9.7i.-.Windows.x64.zip";
+const URL:&str = "https://github.com/SharmaDevanshu089/AutoCrate/releases/download/v0.9.9/v0.9.9.zip";
 const SRS:&str = "There has been a Serious error with the program please use a different version. Crashing";
 const LOGFILE:&str = "install.log";
 const EXECUTIVE_NAME : &str = "USE_INSTALLER_FIRST";
